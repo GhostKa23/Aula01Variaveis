2dc2b950a140fafd13ea892b96681427419c981a
GhostKa23 <kaiquenunes599@gmail.com>
2026-08-12 21:50:38 -0300

    aula 03 - Modificação com menu

--- obj/Debug/net10.0/Aula01variaveis.AssemblyInfo.cs
+++ obj/Debug/net10.0/Aula01variaveis.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula01variaveis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+925cca01e18417c5658cdd7fed2b56836c21d8ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9fdba857bb55ecf3a1cf7f8eb2e3c209a0032b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula01variaveis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula01variaveis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
